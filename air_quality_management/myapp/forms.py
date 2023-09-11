5200b0555f17a2953e09e81973654dfbeea85279
Tran Duy Anh <anh.td.jv@gmail.com>
2023-09-11 11:16:29 +0700

    Registration and login

--- air_quality_management/myapp/forms.py
+++ air_quality_management/myapp/forms.py
@@ -1,44 +1,34 @@
 from django import forms
-import re
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.translation import gettext_lazy as _
-from .models import Profile
+# from .models import Profile
+from django.contrib.auth.forms import UserCreationForm
 
 
-class RegistrationForm(forms.Form):
-    username = forms.CharField(label=_('Username'), max_length=30)
-    email = forms.EmailField(label=_('Email'))
-    password1 = forms.CharField(label=_('Password'), widget=forms.PasswordInput())
-    password2 = forms.CharField(label=_('Re-enter password'), widget=forms.PasswordInput())
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=65)
+    password = forms.CharField(max_length=65, widget=forms.PasswordInput)
 
-    # Kiem tra du lieu nhap lai mat khau
-    # Truoc tien kiem tra password1 nhap chua, neu roi thi lay thong tin password1 va password2
-    # thong qua thuoc tinh cleaned_data de kiem tra.
-    # Neu 2 gia tri bang nhau va khong co dau cach thi xem nhu hop le va return password2
-    # Neu khong thi raise loi "Mat khau khong hop le"
-    def clean_password2(self):
-        if 'password1' in self.cleaned_data:
-            password1 = self.cleaned_data['password1']
-            password2 = self.cleaned_data['password2']
-            if password1 == password2 and password1:
-                return password2
-        raise forms.ValidationError(_("Invalid password"))
 
-    def clean_username(self):
-        username = self.cleaned_data['username']
-        if re.search(r'^\w+&', username):
-            raise forms.ValidationError(_("Username has invalid characters"))
-        try:
-            User.objects.get(username=username)
-        except ObjectDoesNotExist:
-            return username
-        raise forms.ValidationError(_("Account has already existed"))
+class RegisterForm(UserCreationForm):
+    username = forms.CharField(max_length=65,
+                               required=True,
+                               widget=forms.TextInput(attrs={'placeholder': _('Enter your username')}))
+    email = forms.EmailField(max_length=200,
+                             required=True,
+                             widget=forms.EmailInput(attrs={'placeholder': _('Enter your email')}))
+    password1 = forms.CharField(max_length=65,
+                                required=True,
+                                widget=forms.PasswordInput(attrs={'placeholder': _('Create password')}))
+    password2 = forms.CharField(max_length=65,
+                                required=True,
+                                widget=forms.PasswordInput(attrs={'placeholder': _('Confirm password')}))
 
-    def save(self):
-        User.objects.create_user(username=self.cleaned_data['username'],
-                                 email=self.cleaned_data['email'],
-                                 password=self.cleaned_data['password1'])
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
 
 class UpdateUserForm(forms.ModelForm):
     username = forms.CharField(max_length=100,
--- air_quality_management/myapp/views.py
+++ air_quality_management/myapp/views.py
@@ -3,13 +3,16 @@ from .models import AirQualityData, Location, Pollutant, Sensor
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .forms import UpdateUserForm
-from django.contrib import messages
 from django.contrib.auth.views import PasswordChangeView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
-
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.http import HttpResponseRedirect
 from django.views import View
+from .forms import LoginForm, RegisterForm
+
 
 def homepage(request):
     """
@@ -24,31 +27,63 @@ def homepage(request):
     }
     return render(request, 'homepage.html', context)
 
-def login_view(request):
-    """
-    View for the login.
-    """
-    # You can add logic here to fetch data from your models if needed
-    # For now, we'll render the homepage template with some dummy data
-    dummy_data = []
 
-    context = {
-        'data': dummy_data,
-    }
-    return render(request, 'login.html', context)
+def login_view(request):
+    if request.method == 'GET':
+        form = LoginForm()
+        return render(request, 'registration/login.html', {'form': form})
+
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                messages.success(request, f'Welcome back!')
+                return HttpResponseRedirect('/')
+
+        messages.error(request, _('Invalid username or password'))
+        return render(request, 'registration/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, _("You've been logged out."))
+    return HttpResponseRedirect('login')
+
 
 def register_view(request):
+    if request.method == 'GET':
+        form = RegisterForm()
+        return render(request, 'registration/register.html', {'form': form})
+
+    elif request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, _('You have signed up successfully'))
+            login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            return render(request, 'registration/register.html', {'form': form})
+
+
+def user_view(request):
     """
-    View for the login.
+    View to display a list of air quality data.
     """
-    # You can add logic here to fetch data from your models if needed
-    # For now, we'll render the homepage template with some dummy data
     dummy_data = []
 
     context = {
         'data': dummy_data,
     }
-    return render(request, 'register.html', context)
+    return render(request, 'user/user_profile.html', context)
+
 
 class city_details(View):
     template_name = 'city_details.html'
@@ -63,6 +98,7 @@ class city_details(View):
 
         return render(request, self.template_name, context)
 
+
 def air_quality_data_list(request):
     """
     View to display a list of air quality data.
@@ -72,6 +108,7 @@ def air_quality_data_list(request):
         'air_quality_data': air_quality_data,
     }
     return render(request, 'data/air_quality_data_list.html', context)
+
 
 def air_quality_data_detail(request, data_id):
     """
@@ -83,9 +120,11 @@ def air_quality_data_detail(request, data_id):
     }
     return render(request, 'my_app/air_quality_data_detail.html', context)
 
+
 @login_required
 def profile(request):
     return render(request, 'user/user_profile.html')
+
 
 @login_required
 def edit_profile(request):
@@ -100,6 +139,7 @@ def edit_profile(request):
         user_form = UpdateUserForm(instance=request.user)
 
     return render(request, 'user/profile_edit.html', {'user_form': user_form})
+
 
 class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
     template_name = 'user/change_password.html'
