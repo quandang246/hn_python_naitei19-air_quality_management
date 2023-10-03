d0b6342dda7b97db8289d18aba003d565e0d59f2
Phạm Quốc Huy <phamquochuy302002@gmail.com>
2023-10-04 01:12:19 +0700

    67641 admin manage data for city

--- air_quality_management/myapp/admin.py
+++ air_quality_management/myapp/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile,AirQualityData, Pollutant, Sensor
 
 admin.site.register(Profile)
+admin.site.register(Sensor)
+
+class AirQualityDataAdmin(admin.ModelAdmin):
+    list_display = ('city', 'timestamp', 'main_pollutant', 'air_quality_index', 'pol_level', 'provider')
+    list_filter = ('city', 'provider')
+    fields = ['city', ('longitude', 'latitude'), ('main_pollutant', 'pol_level'), ('pollutant_id', 'air_quality_index'), 'provider']
+
+admin.site.register(AirQualityData, AirQualityDataAdmin)
+
+class PollutantAdmin(admin.ModelAdmin):
+    list_display = ('SO2', 'O3', 'PM2_5', 'PM10')
+    
+admin.site.register(Pollutant, PollutantAdmin)
